1b06b0d033e5f0c23b485c2e5b01863b16abbace
Moiz-Adeeb <moizadeebofficial@gmail.com>
2026-01-27 19:46:03 +0500

    image-optimizations

--- src/app/pages/services/components/services-page/services-page.component.ts
+++ src/app/pages/services/components/services-page/services-page.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from "@angular/common";
-import { list } from 'postcss';
-import { List } from 'lodash';
+import { CommonModule } from "@angular/common";
 import { RouterLink } from '@angular/router';
 
 interface service{
@@ -16,38 +14,37 @@ interface service{
   templateUrl: './services-page.component.html',
   styleUrl: './services-page.component.css',
 })
-
 export class ServicesPageComponent {
   services: service[] = [
     {
       label: 'Website Development',
-      img: '/assets/img/website_development.png',
+      img: '/assets/img/services/website_development.webp',
       link: '/website_development',
     },
     {
       label: 'Mobile App Development',
-      img: '/assets/img/mobile_app_development.png',
+      img: '/assets/img/services/mobile_app_development.webp',
       link: '/mobile_app_development',
     },
     {
       label: 'UI/UX Design',
-      img: '/assets/img/ui_ux_design.png',
+      img: '/assets/img/services/ui_ux_design.webp',
       link: '/ui_ux_development',
     },
     {
       label: 'Graphic Designing',
-      img: '/assets/img/graphic_designing.png',
+      img: '/assets/img/services/graphic_designing.webp',
       link: '/graphic_designing',
     },
     {
       label: 'Branding',
-      img: '/assets/img/branding.png',
-      link: '/brandin',
+      img: '/assets/img/services/branding.webp',
+      link: '/branding',
     },
     {
       label: 'Logo Designing',
-      img: '/assets/img/logo_designing.png',
-      link: '/log_designing',
+      img: '/assets/img/services/logo_designing.webp',
+      link: '/logo_designing',
     },
   ];
 }
